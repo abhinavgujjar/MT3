2150f296c6a2aea6648dd8e12f7750a04e8138f4
Abhinav Gujjar <abhinavgujjar@gmail.com>
2015-01-21 14:40:15 +0530

    mongooose and some reorganization

---
+++ app/app.js
@@ -0,0 +1,50 @@
+	//declaring a module
+	var app = angular.module('myApp', []);
+
+	//access a module
+	app.controller('mainController', function($scope){
+		var timeOfDay = 'Morning';
+		var currentHours = (new Date()).getHours();
+		if ( currentHours >= 11 ){
+			timeOfDay = 'Afternoon'
+		}
+
+		if ( currentHours > 16 ){
+			timeOfDay = 'Evening';
+		}
+
+		if (currentHours > 20){
+			timeOfDay = 'Night';
+		}
+		$scope.greeting = 'Good ' + timeOfDay;
+
+		$scope.company = {
+			name : 'Mindtree',
+			location : 'global village'
+		}
+
+		$scope.name = 'World';
+  
+  $scope.city = 'Bangalore';
+  $scope.state = 'Karnataka';
+  
+  $scope.numberOfHotels = '3';
+  
+  $scope.hotels = [{
+      name : 'Leela Palace',
+      price : 30000,
+      img : 'Itxjyso.jpg'
+  },{
+      name : 'Taj Vivanta',
+      price : 15000,
+      img : 'oJG4EEl.jpg'
+  },{
+      name : 'Le Meridian',
+      price : 18000,
+      img : '9DiuKqi.jpg'
+  }];
+  
+  
+	})
+
+	
