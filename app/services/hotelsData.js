001390a326b95295cf5936be7032d616b7fb90d3
Abhinav Gujjar <abhinavgujjar@gmail.com>
2015-01-22 09:58:43 +0530

    ADDED AREA property

--- app/services/hotelsData.js
+++ app/services/hotelsData.js
@@ -17,7 +17,8 @@ app.factory('hotelsData', function() {
 			"description": "Very comfortable, clean and classy hotel",
 			"website": "http://www.clarksexotica.com/",
 			"img": "clarks-exotica-resort.jpg",
-			"rating": 6
+			"rating": 6,
+			"area" : 550000
 		}, {
 			"id": "3",
 			"name": "The Elanza Hotel",
@@ -26,7 +27,8 @@ app.factory('hotelsData', function() {
 			"description": "Stayed at the Elanza for a night on a business trip. Affordable luxury without much fuss. Rooms have every modern convenience with top notch fittings in the bathroom, fantastic rain shower, electronic keypad based controls for lights etc., very comfortable bed, great lighting etc. The lobby is nice but not gigantic like that of a 5-star hotel but elegantly sufficient. The restaurant is fine for a night's dinner and a decent breakfast - although the food quality was fine it could stand some improvement. Friendly, attentive staff. Good location. Will stay there again.",
 			"website": "http://www.clarksexotica.com/",
 			"img": "the-elanza-hotel.jpg",
-			"rating": 3
+			"rating": 3,
+			"area" : 700000
 		}, {
 			"id": "4",
 			"name": "Ramada Bangalore",
@@ -35,7 +37,8 @@ app.factory('hotelsData', function() {
 			"description": "I stayed for two nights in this hotel. The hotel was right at the heart of the city near by commercial street. The room was neat clean especially the bathroom was so hygenic. The complementary breakfast buffet was simply awesome and delicious. Here I wud say that they match a five star hotel. About the cons, the staffs during check in were not bothered about welcoming, with no one to even carry the luggages it was a mere rude welcome I must say. The restaurant food there if u must have it is so expensive so better to dine out. Overall it is a very good stay. Cheers to Ramada people",
 			"website": "http://www.clarksexotica.com/",
 			"img": "ramada.jpg",
-			"rating": 8
+			"rating": 8,
+			"area" : 10000
 		}, {
 			"id": "5",
 			"name": "Ibis",
@@ -44,7 +47,8 @@ app.factory('hotelsData', function() {
 			"description": "I had a terrible experience of staying at Ibis Hosur road for two nights. Had expected a upmarket hotel as the rates at Ibis was equivalent to those charged by other 3 star hotels. Issued faced : Airconditioning wasn't working in the first night. Had to call reception at 4 am and asked for a single bedsheet as it was hot. Only one common low end iron box along with stand available across all floors. The iron box works only at a particular setting. I had to do a quick iron in the second day morning and had to call the room service to get my shirt ironed. Free WIFI is advertised. But it is only 256kbps connection. Rooms are pretty small and chairs were dirty. Even you need to swipe your room access card to use the life. First time I am facing this. The second day morning, I met the manager and complained about the airconditioning. She agreed to have it looked into. But when I came in the night, nothing was changed. After complaining again, they sent the technician. First he said that nothing is wrong. I called reception again and then he checked the system again and rectified the problem (blockage in the air vents). Ibis at Hosur road is not worth the amount we are paying unless you don't get better deals else where. Won't stay there again. Stayed November 2014, travelled on business",
 			"website": "http://www.clarksexotica.com/",
 			"img": "ibis.jpg",
-			"rating": 1
+			"rating": 1,
+			"area" : 564228
 		}, {
 			"id": "6",
 			"name": "The Oberoi",
@@ -53,7 +57,8 @@ app.factory('hotelsData', function() {
 			"description": "From the moment you walk into the Oberoi Bangalore, you are treated like royalty. The staff takes care of every single need and anticipates your potential needs. The food in the restaurants tastes fabulous and is also very creative. The décor and cleanliness of the hotel is also outstanding. Will definitely be back!",
 			"website": "http://www.oberoi.com/",
 			"img": "oberoi.jpg",
-			"rating": 10
+			"rating": 10,
+			"area" : 340009
 		}, {
 			"id": "7",
 			"name": "ALOFT",
@@ -62,7 +67,8 @@ app.factory('hotelsData', function() {
 			"description": "Convenient",
 			"website": "http://www.oberoi.com/",
 			"img": "trinity-suites.jpg",
-			"rating": 10
+			"rating": 10,
+			"area" : 457779
 		}];
 
 		return {
